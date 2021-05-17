c7f48c8085d5f7f7bdab40b625cb3faee021aa77
MellowYarker <milan.miladinovic@mail.utoronto.ca>
2021-05-17 13:38:34 -0400

    Changed up the account struct and methods

--- src/account.rs
+++ src/account.rs
@@ -1,30 +1,32 @@
 use crate::exchange::requests::Order;
+use crate::exchange::filled::FilledOrder;
 use std::collections::HashMap;
 
 // Stores data about a user
 pub struct UserAccount {
-    username: String,
-    password: String,
-    id: Option<i32>,
-    buys: Vec<Order>,
-    sells: Vec<Order>
+    pub username: String,
+    pub password: String,
+    pub id: Option<i32>,
+    pub placed_orders: Vec<Order>,
+    pub trades: Vec<FilledOrder>
 }
 
 // Where we store all our users
 pub struct Users {
-    users: HashMap<String, UserAccount>,
-    total: i32
+    pub users: HashMap<String, UserAccount>,
+    pub total: i32
 }
 
 impl UserAccount {
     pub fn from(name: &String, password: &String) -> Self {
-        let vec: Vec<Order> = Vec::new();
+        let placed: Vec<Order> = Vec::new();
+        let trades: Vec<FilledOrder> = Vec::new();
         UserAccount {
             username: name.to_string().clone(),
             password: password.to_string().clone(),
-            id: None, // Set later
-            buys: vec.clone(),
-            sells: vec,
+            id: None, // We set this later
+            placed_orders: placed,
+            trades: trades,
         }
     }
 
@@ -36,7 +38,6 @@ impl UserAccount {
 }
 
 impl Users {
-
     pub fn new() -> Self {
         let map: HashMap<String, UserAccount> = HashMap::new();
         Users {
@@ -55,6 +56,63 @@ impl Users {
             self.total = account.set_id(&self);
             self.users.insert(account.username.clone(), account);
             return Some(self.total);
+        }
+    }
+
+    /* Returns a reference to a user account if:
+     *  - the account exists and
+     *  - the password is correct for this user
+     */
+    pub fn get(&self, username: &String, password: &String) -> Option<&UserAccount> {
+        match self.users.get(username) {
+            Some(account) => {
+                if *password == account.password {
+                    return Some(account);
+                }
+                println!("Incorrect password for username ({})", username);
+                return None;
+            },
+            None => {
+                println!("The provided username doesn't exist in our records.");
+                return None;
+            }
+        }
+    }
+
+    /* Returns a mutable reference to a user account if:
+     *  - the account exists and
+     *  - the password is correct for this user
+     */
+    pub fn get_mut(&mut self, username: &String, password: &String) -> Option<&mut UserAccount> {
+        match self.users.get_mut(username) {
+            Some(account) => {
+                if *password == account.password {
+                    return Some(account);
+                }
+                println!("Incorrect password for username ({})", username);
+                return None;
+            },
+            None => {
+                println!("The provided username doesn't exist in our records.");
+                return None;
+            }
+        }
+    }
+
+    /* Prints the account information of this user if:
+     *  - the account exists and
+     *  - the password is correct for this user
+     */
+    pub fn print_user(&self, username: &String, password: &String) {
+        if let Some(account) = self.get(username, password) {
+            println!("Orders Awaiting Execution");
+            for order in account.placed_orders.iter() {
+                println!("{:?}", order);
+            }
+            println!("\nExecuted Trades");
+            for order in account.trades.iter() {
+                println!("{:?}", order);
+            }
         }
     }
 }
