e628084ac4fe9c8c077ffb11a7682f5454f7f434
MellowYarker <milan.miladinovic@mail.utoronto.ca>
2021-05-17 19:59:03 -0400

    Don't allow a user to fill their own buy or sell order

--- src/exchange/market.rs
+++ src/exchange/market.rs
@@ -41,6 +41,11 @@ impl Market {
                 None => return new_price // No more sell orders to fill
             };
 
+            // We don't allow a user to buy their own sell order.
+            if highest_bid.username == lowest_offer.username {
+                continue;
+            }
+
             let lowest_sell_remaining = lowest_offer.quantity - lowest_offer.filled;
             let highest_bid_remaining = highest_bid.quantity - highest_bid.filled;
 
@@ -113,6 +118,11 @@ impl Market {
                 Some(bid) => bid,
                 None => return new_price // No more buy orders to fill
             };
+
+            // We don't allow a user to sell into their own buy order.
+            if highest_bid.username == lowest_offer.username {
+                continue;
+            }
 
             let lowest_sell_remaining = lowest_offer.quantity - lowest_offer.filled;
             let highest_bid_remaining = highest_bid.quantity - highest_bid.filled;
